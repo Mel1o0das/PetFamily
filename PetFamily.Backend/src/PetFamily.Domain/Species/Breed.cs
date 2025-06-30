8a6d8db6811220d195c464547d29049fd2dc8181
Mel1o0das <m4rsik2016@yandex.ru>
2025-06-30 16:41:36 +0300

    Добавлены сущности Breed и Species

---
+++ PetFamily.Backend/src/PetFamily.Domain/Species/Breed.cs
@@ -0,0 +1,30 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.Species;
+
+public class Breed : Shared.Entity<BreedId>
+{
+    // ef core
+    private Breed(BreedId breedId)
+        : base(breedId)
+    {
+    }
+
+    private Breed(BreedId breedId, string name)
+        : base(breedId)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+
+    public static Result<Breed> Create(string name)
+    {
+        if(string.IsNullOrWhiteSpace(name))
+            return Result.Failure<Breed>("Name cannot be empty");
+
+        var breed = new Breed(BreedId.NewBreedId(), name);
+        
+        return Result.Success(breed);
+    }
+}
---
+++ PetFamily.Backend/src/PetFamily.Domain/Species/Species.cs
@@ -0,0 +1,34 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace PetFamily.Domain.Species;
+
+public class Species : Shared.Entity<SpeciesId>
+{
+    private readonly List<Breed> _breeds = [];
+    
+    // ef core
+    private Species(SpeciesId id) 
+        : base(id)
+    {
+    }
+
+    private Species(SpeciesId id, string name)
+        : base(id)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+    
+    public IReadOnlyList<Breed> Breeds => _breeds;
+
+    public static Result<Species> Create(string name)
+    {
+        if(string.IsNullOrWhiteSpace(name))
+            return Result.Failure<Species>("Name cannot be empty");
+        
+        var species = new Species(SpeciesId.NewSpeciesId(), name);
+        
+        return Result.Success(species);
+    }
+}
