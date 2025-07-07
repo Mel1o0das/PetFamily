a06796c62aa4839087d8377189ca05b41abd2b03
Mel1o0das <m4rsik2016@yandex.ru>
2025-07-07 18:05:10 +0300

    Добавлены SecretKey, прописаны конфигурации efcore

---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Configurations/PetConfiguration.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Domain.Pets;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Infrastructure.Configurations;
+
+public class PetConfiguration : IEntityTypeConfiguration<Pet>
+{
+    public void Configure(EntityTypeBuilder<Pet> builder)
+    {
+        builder.ToTable("pets");
+        
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Id)
+            .HasConversion(
+                id => id.Value,
+                value => PetId.Create(value));
+        
+        builder.Property(p => p.Name)
+            .IsRequired()
+            .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH);
+        
+        builder.Property(p => p.Description)
+            .IsRequired()
+            .HasMaxLength(Constants.MAX_HIGH_TEXT_LENGTH);
+
+        builder.OwnsOne(p => p.SpeciesBreed, pb =>
+        {
+            pb.Property(p => p.BreedId).IsRequired().HasColumnName("breed_id");
+            pb.Property(p => p.SpeciesId).IsRequired().HasColumnName("species_id");
+        });
+        
+        builder.Property(p => p.Color).IsRequired();
+
+        builder.ComplexProperty(p => p.InformationAboutHealth, pb =>
+        {
+            pb.Property(p => p.Description)
+                .IsRequired()
+                .HasMaxLength(Constants.MAX_HIGH_TEXT_LENGTH);
+
+            pb.Property(p => p.Height).IsRequired();
+            pb.Property(p => p.Weight).IsRequired();
+            pb.Property(p => p.IsCastrated).IsRequired();
+            pb.Property(p => p.IsVaccinated).IsRequired();
+        });
+
+        builder.ComplexProperty(p => p.Address, pb =>
+        {
+            pb.Property(p => p.City)
+                .IsRequired()
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH);
+            
+            pb.Property(p => p.Street)
+                .IsRequired()
+                .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH);
+            
+            pb.Property(p => p.StreetNumber).IsRequired();
+        });
+        
+        builder.Property(p => p.PhoneNumber).IsRequired();
+        
+        builder.Property(p => p.DateOfBirth).IsRequired();
+        
+        builder.Property(p => p.Status).IsRequired();
+
+        builder.ComplexProperty(p => p.DetailsForHelp, pb =>
+        {
+            pb.Property(p => p.Description)
+                .IsRequired()
+                .HasMaxLength(Constants.MAX_HIGH_TEXT_LENGTH);
+            
+            pb.Property(p => p.Requisites).IsRequired();
+        });
+        
+        builder.Property(p => p.DateCreated).IsRequired();
+    }
+}
---
+++ PetFamily.Backend/src/PetFamily.Infrastructure/Configurations/VolunteerConfiguration.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PetFamily.Domain.Shared;
+using PetFamily.Domain.Volunteers;
+
+namespace PetFamily.Infrastructure.Configurations;
+
+public class VolunteerConfiguration : IEntityTypeConfiguration<Volunteer>
+{
+    public void Configure(EntityTypeBuilder<Volunteer> builder)
+    {
+        builder.ToTable("volunteers");
+        
+        builder.HasKey(v => v.Id);
+
+        builder.Property(v => v.Id)
+            .HasConversion(
+                id => id.Value,
+                value => VolunteerId.Create(value));
+
+        builder.ComplexProperty(v => v.InformationAboutVolunteer, vb =>
+        {
+            vb.Property(v => v.Surname).IsRequired();
+            vb.Property(v => v.Name).IsRequired();
+            vb.Property(v => v.Patronymic).IsRequired();
+            vb.Property(v => v.Email).IsRequired();
+            vb.Property(v => v.PhoneNumber).IsRequired();
+            vb.Property(v => v.Experience).IsRequired();
+        });
+        
+        builder.Property(v => v.Description)
+            .IsRequired()
+            .HasMaxLength(Constants.MAX_HIGH_TEXT_LENGTH);
+
+        builder.OwnsOne(
+            v => v.SocialNetworksDetails,
+            vb =>
+            {
+                vb.ToJson();
+                vb.OwnsMany(sn => sn.SocialNetworks, sb =>
+                {
+                    sb.Property(sn => sn.Name)
+                        .IsRequired();
+                    sb.Property(sn => sn.Link)
+                        .IsRequired();
+                });
+            });
+
+        builder.ComplexProperty(v => v.DetailsForHelp, vb =>
+        {
+            vb.Property(v => v.Description)
+                .IsRequired()
+                .HasMaxLength(Constants.MAX_HIGH_TEXT_LENGTH);
+            
+            vb.Property(v => v.Requisites).IsRequired();
+        });
+
+        builder
+            .HasMany(v => v.Pets)
+            .WithOne()
+            .HasForeignKey("volunteer_id");
+    }
+}
