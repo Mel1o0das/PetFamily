fa43aa2458a87dc28b4c11e3c1681b95ddc44080
Mel1o0das <m4rsik2016@yandex.ru>
2025-07-18 22:34:46 +0300

    Добавлены VO и regex проверка для Email и PhoneNumber

---
+++ PetFamily.Backend/src/PetFamily.Domain/Volunteers/Email.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using CSharpFunctionalExtensions;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Domain.Volunteers;
+
+public record Email
+{
+    private Email(string emailAddress)
+    {
+        EmailAddress = emailAddress;
+    }
+
+    public string EmailAddress { get; }
+
+    public static Result<Email, Error> Create(string emailAddress)
+    {
+        var email = new Regex(Constants.Email.PATTERN);
+        
+        if(!email.IsMatch(emailAddress))
+            return Errors.General.ValueIsInvalid("email");
+        
+        return new Email(emailAddress);
+    }
+}
